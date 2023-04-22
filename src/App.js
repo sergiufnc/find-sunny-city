b987612e26ab878e6e1b1d981dc16f706d511c82
Sergiu Finciuc <serfnc@icloud.com>
2023-04-22 12:35:03 +0200

    button added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // create a button that will call a function that returns a random number
+  // between 1 and 100
+
+  // create a function that returns a random number between 1 and 100
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  // create a function that will be called when the button is clicked
+  function handleClick() {
+    // call the getRandomNumber function and store the result in a variable
+    const randomNumber = getRandomNumber();
+    // log the result to the console
+    console.log(randomNumber);
+  }
+
+  // create a button that will call the handleClick function when clicked
+  function Button() {
+    return <button onClick={handleClick}>Click me!</button>;
+  }
+
+  // render the button
+  return <Button />;
+
   return (
     <div className="App">
       <header className="App-header">
